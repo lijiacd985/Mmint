e91ae25a284ec2bbcadbf1b547ef6416c93f57c4
lijiacd985 <lijiacd@gmail.com>
2017-05-30 17:09:11 -0500

    Update PCA.meth.py

--- PCA.meth.py
+++ PCA.meth.py
@@ -44,7 +44,7 @@ X_r = pca.fit(X).transform(X)
 x = np.arange(int(args.number))
 ys = [i+x+(i*x)**2 for i in range(int(args.number))]
 colors = cm.rainbow(np.linspace(0, 1, len(ys)))
-for c, i, name, markers in zip(colors, np.arange(0,int(args.number)+2), names, ('o', 'o','o','^','^','^')):
+for c, i, name, markers in zip(colors, np.arange(0,int(args.number)+2), names, ('^', '^','^','^','^','^','^', '^','^','^','^','^','^', '^','^','^','^','^','^', '^','^','^','^','^')):
 	plt.scatter(X_r[Y == i, 0], X_r[Y == i, 1], c=c, label=name,alpha=0.8,marker=markers,s=80)
 plt.legend(loc=2)
 plt.savefig(args.output+".pdf")
