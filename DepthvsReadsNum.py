927764511525f1e825f95e8fee98798cf1a5a71e
Dangertrip <dangertrip@hotmail.com>
2017-07-18 21:24:55 -0500

    0.0.1

--- DepthvsReadsNum.py
+++ DepthvsReadsNum.py
@@ -14,18 +14,19 @@ import seaborn as sns
 import subprocess
 
 
-sns.set(font_scale=1.5)
+#sns.set(font_scale=1.5)
 
 
-ratio=[]
-cov=[]
-sum=[]
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-m','--methfile',help="The output from mcall: *.G.bed",nargs="*", metavar="FILE")
-parser.add_argument('-l','--label',help="The labels",nargs="*", metavar="FILE")
-parser.add_argument('-o','--output',help="The output file",nargs="*", metavar="FILE")
-args = parser.parse_args()
+#ratio=[]
+#cov=[]
+#sum=[]
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m','--methfile',help="The output from mcall: *.G.bed",nargs="*", metavar="FILE")
+    parser.add_argument('-l','--label',help="The labels",nargs="*", metavar="FILE")
+    parser.add_argument('-o','--output',help="The output file",nargs="*", metavar="FILE")
+    run(parser)
+#args = parser.parse_args()
 #print args.methfile
 
 
@@ -61,56 +62,60 @@ subprocess.call("rm tmp", shell=True)
 
 fig.savefig(args.output[0]+"-Ratio.pdf")
 '''
-
-c=['r','b','g','y','c','m','k']
-fig,ax = plt.subplots()
-plt.xlim(0,11)
-
-t=[]
-#with open("tmp", "w") as outfile:
-for i in range(len(args.methfile)):
-    with open("tmp", "w") as outfile:   
-        subprocess.call("cut -f1-5 %s | sed '1d'" % args.methfile[i], shell=True,stdout=outfile)
-    methfile=open("tmp",'r')
-    next(methfile)
-    for line1 in methfile:
-        rows1=line1.strip().split("\t")
-        #ratio.append(float(rows1[3]))
-        cov.append(int(rows1[4]))
-    #a=[1,2,3,4,5,6]
-    b=[]
-    s=[]
-    S=0
-    St=0
-
-    #print cov
-    for n in range(10):
-        for x in cov:
-	    St+=x
-            if (x>=2**n):
-                S+=x
-        #print St
-        #print S
-        s.append(int(S)/float(St)*100)
-        St = 0
-     #   print b   
-        S=0 
-    t.append(s)
+def run(parser):
+    args = parser.parse_args()
+    sns.set(font_scale=1.5)
+    ratio=[]
     cov=[]
-    s=[]
-    subprocess.call("rm tmp", shell=True)
+    sum=[]
+    c=['r','b','g','y','k','c','m']
+    fig,ax = plt.subplots()
+    plt.xlim(0,11)
+    t=[]
+    #with open("tmp", "w") as outfile:
+    for i in range(len(args.methfile)):
+        with open("tmp", "w") as outfile:
+            subprocess.call("cut -f1-5 %s | sed '1d'" % args.methfile[i], shell=True,stdout=outfile)
+        methfile=open("tmp",'r')
+        next(methfile)
+        for line1 in methfile:
+            rows1=line1.strip().split("\t")
+            #ratio.append(float(rows1[3]))
+            cov.append(int(rows1[4]))
+        #a=[1,2,3,4,5,6]
+        b=[]
+        s=[]
+        S=0
+        St=0
+
+        #print cov
+        for n in range(10):
+            for x in cov:
+	        St+=x
+                if (x>=2**n):
+                    S+=x
+            #print St
+            #print S
+            s.append(int(S)/float(St)*100)
+            St = 0
+        #   print b
+            S=0
+        t.append(s)
+        cov=[]
+        s=[]
+        subprocess.call("rm tmp", shell=True)
 
 
 #print t[1]
 
-for i in range(len(args.methfile)):
-    xaxis=[1,2,3,4,5,6,7,8,9,10]
-    labels=[1,2,4,8,16,32,64,128,256,512]
-    plt.plot(xaxis,t[i],'-o',color=c[i],label=args.label[i])
-    plt.legend(loc="lower left")
-plt.xticks(xaxis, labels)
-plt.xlabel('Coverage (>=)')
-#plt.ylabel('Reads Number Percentage')
-plt.ylabel('Wig sum for selected CpGs (Percentage)')
+    for i in range(len(args.methfile)):
+        xaxis=[1,2,3,4,5,6,7,8,9,10]
+        labels=[1,2,4,8,16,32,64,128,256,512]
+        plt.plot(xaxis,t[i],'-o',color=c[i],label=args.label[i])
+        plt.legend(loc="lower left")
+    plt.axvline(x=100,color='red',linewidth=2,linestyle='dashed')
+    plt.xticks(xaxis, labels)
+    plt.xlabel('Coverage (>=)')
+    plt.ylabel('Wig sum for selected CpGs (Percentage)')
 
-fig.savefig(args.output[0]+"-Cov.pdf")
+    fig.savefig(args.output[0]+"-Cov.pdf")
