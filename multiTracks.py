4e3072ca934a11e7cf2ddf4c813ab5b385fbc193
jiali@dsunlab.srv.tamhsc.edu <jiali@dsunlab.srv.tamhsc.edu>
2017-11-16 10:02:19 -0600

    updata multiTracks

--- multiTracks.py
+++ multiTracks.py
@@ -53,17 +53,20 @@ def run(parser):
         for i in range(len(args.bigwig)):
             subprocess.call("multiBigwigSummary bins -bs %s -r %s -b %s --labels %s -out %s --outRawCounts %s" % (args.binsize, array1[k],args.bigwig[i],args.labels[i],out[i],RawCounts[i]),shell=True)
         for i in range(len(args.bigwig)):
-    		subprocess.call("sed -i 's/nan/0/g' %s" % RawCounts[i], shell=True)
+    	    subprocess.call("sed -i 's/nan/0/g' %s" % RawCounts[i], shell=True)
         for i in range(len(args.bigwig)):
             subprocess.call("sh format.shareX.sh %s" % RawCounts[i],shell=True)
         dt = pd.read_table("merge.clean",header=1)
         x_sub_flat = dt.as_matrix(columns=dt.columns[2:3])
+        print 'A'
         x_trans = map(list,zip(*x_sub_flat))
+        
         y_sub = [a for a in dt.as_matrix(columns = dt.columns[3:dt.shape[1]])]
         y_trans = map(list,zip(*y_sub))
         plt.close('all')
         x_smooth = np.linspace(min(x_trans[0]), max(x_trans[0]), 1000)
         y_smooth=[]
+        print len(y_trans)
         for i in range(len(y_trans)):
             y_smooth.append(spline(x_trans[0], y_trans[i], x_smooth))
             ymax.append(max(y_trans[i]))
@@ -75,10 +78,11 @@ def run(parser):
                 mmax[i] = max(mmax[i],np.max(y_smooth[i*args.replicate+j]))
         print mmax
         for i,v in enumerate(xrange(len(y_trans))):
+            
             v=v+1
             ax1 = subplot(len(y_trans),1,v)
             ax1.plot(x_smooth,y_smooth[i],color='white',linewidth=0.1)
-            #print i,v
+            print i,v
             if args.meth:
                 ax1.set_ylim(0,1.1)
             else:
@@ -102,5 +106,17 @@ def run(parser):
         ymax=[]
         fig.savefig(str(array2[k])+'.pdf')
 
+
+
 if __name__=="__main__":
-    print 'aa'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b','--bigwig',help="input file (bw format)",nargs="*", metavar="FILE")
+    parser.add_argument('-labels','--labels',help="labels for the input files",nargs="*", metavar="FILE")
+    parser.add_argument('-c','--color',help="colors for the input file tracks",nargs="*", metavar="FILE")
+    parser.add_argument('-bs','--binsize',help="bins size",metavar="FILE")
+    parser.add_argument('-after','--downstream',help="downstream regions to plot",metavar="FILE")
+    parser.add_argument('-before','--upstream',help="upstream regions to plot",metavar="FILE")
+    parser.add_argument('-r','--regions',help="regions to plot",metavar="FILE")
+    parser.add_argument('-f','--file',help="file include all the regions to plot",metavar="FILE")
+    parser.add_argument('-M','--meth',help="If bw file is methylation file, use -M.",action="store_true")
+    multiTracks(parser) 
