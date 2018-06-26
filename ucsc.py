1730576dc08454d662044a978152ecea51ed4a02
dangertrip <dangertrip@hotmail.com>
2018-06-26 16:11:24 -0500

    ucsc

---
+++ ucsc.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+
+def run(parser):
+    '''
+    -t type
+    -g genome
+    -s scale
+    -o output
+    '''
+    args = parser.parse_args()
+    #files genome scale output
+    #os.system('mkdir ')
+    os.system("perl ucsc_tracks.pl -t "+str(args.type)+" -g "+str(args.genome)+" -s " \
+            +args.scale+" -o "+str(args.output))
+
